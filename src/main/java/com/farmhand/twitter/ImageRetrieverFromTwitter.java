f5285460b6aa2d7256cb4eeee62daae290b8af84
HelmutSteiner <helmut.steinersti2@gmail.com>
2017-07-14 21:45:13 +0200

    Image retriever from Twitter.

---
+++ src/main/java/com/farmhand/twitter/ImageRetrieverFromTwitter.java
@@ -0,0 +1,32 @@
+package com.farmhand.twitter;
+
+import com.sun.prism.Image;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Helmut
+ */
+
+@Service
+public class ImageRetrieverFromTwitter {
+     private final Logger logger = LoggerFactory.getLogger(getClass());
+     
+     public void tweet(Twitter twitter, String text) {
+        try {
+         twitter.timelineOperations().updateStatus(text);
+      } catch (RuntimeException ex) {
+         logger.error("Unable to tweet" + text, ex);
+      }
+    }
+     
+     public void retrieveImage(Twitter twitter, Image img) {
+         
+     }
+}
+    
+    
+
