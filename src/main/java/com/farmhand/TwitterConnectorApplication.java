8c281bb09cdce34c0246e3e64729398eb403962b
HelmutSteiner <helmut.steinersti2@gmail.com>
2017-07-14 20:50:40 +0200

    Initial commit

---
+++ src/main/java/com/farmhand/TwitterConnectorApplication.java
@@ -0,0 +1,12 @@
+package com.farmhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterConnectorApplication.class, args);
+	}
+}
