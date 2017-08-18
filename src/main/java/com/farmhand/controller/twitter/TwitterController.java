6a91afba9141ea39794309b700255cd06347c99b
HelmutSteiner <helmut.steinersti2@gmail.com>
2017-08-18 20:40:28 +0200

    Rest controller for Twitter.

---
+++ src/main/java/com/farmhand/controller/twitter/TwitterController.java
@@ -0,0 +1,27 @@
+package com.farmhand.controller.twitter;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Helmut
+ */
+@RestController
+@RequestMapping(TwitterController.TWITTER_BASE_URI)
+public class TwitterController {
+    public static final String TWITTER_BASE_URI = "farmhand/v1/tweets";
+    
+    @Autowired
+    private Twitter twitter;
+    
+    public List<Tweet> getTweets(@PathVariable final String farmhand) {
+        
+    } 
+    
+    
+}
