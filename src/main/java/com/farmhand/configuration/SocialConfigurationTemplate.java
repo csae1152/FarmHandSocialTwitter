af5984cce60dc5b4d67537aa9186a22551482f54
HelmutSteiner <helmut.steinersti2@gmail.com>
2017-08-18 19:17:22 +0200

    Added new abstract configuration template.

---
+++ src/main/java/com/farmhand/configuration/SocialConfigurationTemplate.java
@@ -0,0 +1,31 @@
+package com.farmhand.configuration;
+
+import com.farmhand.twitter.template.SocialTemplateConnector;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ *
+ * @author Helmut
+ * 
+ * <p>For accessing Twitter and Instagram</p>
+ */
+
+@Configuration
+@PropertySource("classpath:config.properties")
+public class SocialConfigurationTemplate {
+    
+    /**
+     * 
+     * @return TwitterConfigTemplate
+     * @throws Exception 
+     */
+    @Bean
+    public SocialTemplateConnector twitterConfigTemplate() throws Exception {
+        return null;
+        
+        
+    }
+    
+}
