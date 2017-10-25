8963c4a1cf3d2df86071515fb07ec746144c930f
Helmut Steiner <csae1152@uibk.ac.at>
2017-10-25 12:28:58 +0200

    Fix.

--- src/main/java/com/farmhand/TwitterConnectorApplication.java
+++ src/main/java/com/farmhand/TwitterConnectorApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableConfig("dev")
+@EnableEureka
+@EnableZuul
 public class TwitterConnectorApplication {
 
 	public static void main(String[] args) {
